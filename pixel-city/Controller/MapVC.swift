c8baa3c6a90f0891255da8d087e48daccb76cf33
Mariah Baysic <mjpb123@gmail.com>
2020-05-02 12:06:42 +0800

    Info.plist: added Privacy - Locations; MapVC.swift: added request for location authorization, get user location, center on user location

--- pixel-city/Controller/MapVC.swift
+++ pixel-city/Controller/MapVC.swift
@@ -8,21 +8,53 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class MapVC: UIViewController {
     
     @IBOutlet weak var mapView: MKMapView!
     
+    let authorizationStatus = CLLocationManager.authorizationStatus()
+    let regionRadius : Double = 1000
+    
+    var locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         mapView.delegate = self
+        locationManager.delegate = self
+        
+        configureLocationServices()
     }
 
     @IBAction func centerMapBtnPressed(_ sender: Any) {
+        if authorizationStatus == .authorizedAlways || authorizationStatus == .authorizedWhenInUse {
+            centerMapOnUserLocation()
+        }
     }
     
 }
 
 extension MapVC: MKMapViewDelegate {
+    func centerMapOnUserLocation() {
+        guard let coordinate = locationManager.location?.coordinate else { return }
+        let coordinateRegion = MKCoordinateRegion.init(center: coordinate, latitudinalMeters: regionRadius * 2.0, longitudinalMeters: regionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
     
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        centerMapOnUserLocation()
+    }
+}
+
+extension MapVC: CLLocationManagerDelegate {
+    func configureLocationServices() {
+        if authorizationStatus == .notDetermined {
+            locationManager.requestAlwaysAuthorization()
+//            locationManager.requestWhenInUseAuthorization()
+        } else {
+            return
+        }
+    }
 }
